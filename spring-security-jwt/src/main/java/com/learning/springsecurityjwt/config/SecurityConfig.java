ce8335a3451c82e0c6f4c8d1197b9c1ec4d79a4a
Rohini Shinge <shingerohini712@gmail.com>
2022-02-08 20:11:50 +0530

    Added JWT Filter and Config to validate it for each request

--- spring-security-jwt/src/main/java/com/learning/springsecurityjwt/config/SecurityConfig.java
+++ spring-security-jwt/src/main/java/com/learning/springsecurityjwt/config/SecurityConfig.java
@@ -3,13 +3,16 @@ package com.learning.springsecurityjwt.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.learning.springsecurityjwt.filter.JWTRequestFilter;
 import com.learning.springsecurityjwt.service.MyUserDetailsService;
 
 @EnableWebSecurity
@@ -17,6 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private MyUserDetailsService userDetailService;
+	
+	@Autowired
+	private JWTRequestFilter jwtRequestFilter;
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception
@@ -27,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeHttpRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+		http.addFilterBefore(jwtRequestFilter,UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean
