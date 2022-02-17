0b1dc19bb3433781140b395d4269d22ed170fb0a
unknown <shingerohini712@gmail.com>
2022-02-17 23:01:20 +0530

    Added base structure for LDAP Impl

---
+++ spring-security-ldap/src/test/java/com/learning/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
