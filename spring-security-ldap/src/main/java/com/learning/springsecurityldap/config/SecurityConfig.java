7cb74b21a4db2c5ec0a2ff538c844f1be0067f0a
unknown <shingerohini712@gmail.com>
2022-02-17 23:45:47 +0530

    Changes in security config

--- spring-security-ldap/src/main/java/com/learning/springsecurityldap/config/SecurityConfig.java
+++ spring-security-ldap/src/main/java/com/learning/springsecurityldap/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.learning.springsecurityldap.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
